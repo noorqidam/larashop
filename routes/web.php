38b2ddf14b12df7d13e0b807a4ff47850920008b
noorqidam <noorqidam@gmail.com>
2021-02-19 16:30:33 +0700

    Change Routes Home

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\Admin\SlideController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\OrderController as OrderFrontEndController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ProfileController;
@@ -29,16 +30,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/products', [ProductFrontEndController::class, 'index']);
 Route::get('/product/{slug}', [ProductFrontEndController::class, 'show']);
 Route::get('/products/quick-view/{slug}', [ProductFrontEndController::class, 'quickView']);
 
 Route::get('/carts', [CartController::class, 'index']);
-Route::delete('/carts/remove/{cartID}', [CartController::class, 'destroy']);
+Route::get('/carts/remove/{cartID}', [CartController::class, 'destroy']);
 Route::post('/carts', [CartController::class, 'store']);
 Route::post('/carts/update', [CartController::class, 'update']);
 
@@ -115,4 +116,4 @@ Route::prefix('admin')
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
